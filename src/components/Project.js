447c0be9977088b05e25cda57fbe49141e57ec9b
ahmed abbassi <abbassia082@gmail.com>
2024-04-09 15:34:15 +0100

    changes in style

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function Project({image1, technologies}) {
+function Project({image, technologies}) {
   return (
-    <div  className='project'>
-            <img alt='' src={image1} />
+    <a href='https://mellifluous-caramel-db6396.netlify.app/'  className='project'>
+            <img alt='' src={image} />
             <div className='aboutProject'>
               <h3>Chat App</h3>
               <p className='description'> e js and express Js. I excel in integrating responsive websites that offer a smooth user experience,engaging interfaces through writing clean and optimizes code and utilizing cutting-edge developpe this is the project description</p>
@@ -19,7 +19,7 @@ function Project({image1, technologies}) {
                 <span className='toConsult'>Live Demo</span>
               </div>
             </div>
-          </div>
+          </a>
   )
 }
 
